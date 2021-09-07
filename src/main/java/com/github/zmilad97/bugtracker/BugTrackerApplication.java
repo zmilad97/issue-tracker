b6e1332d200b8709e718760194a1fe6711a1dd57
milad <zmilad97@gmail.com>
2021-09-07 17:13:46 +0430

    init

---
+++ src/main/java/com/github/zmilad97/bugtracker/BugTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.zmilad97.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/github/zmilad97/bugtracker/model/User.java
@@ -0,0 +1,96 @@
+package com.github.zmilad97.bugtracker.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(nullable = false, unique = true, updatable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private String roles;
+    private String permissions;
+    private boolean isActive;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
