de2eb693308224c8c5304ae151874eaf07ed319a
milad <zmilad97@gmail.com>
2021-09-19 17:49:23 +0430

    added methods in ProjectController

--- src/main/java/com/github/zmilad97/bugtracker/controller/ProjectController.java
+++ src/main/java/com/github/zmilad97/bugtracker/controller/ProjectController.java
@@ -1,4 +1,74 @@
 package com.github.zmilad97.bugtracker.controller;
 
+import com.github.zmilad97.bugtracker.dtos.ProjectDto;
+import com.github.zmilad97.bugtracker.security.SecurityUtil;
+import com.github.zmilad97.bugtracker.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
 public class ProjectController {
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/projects")
+    public ModelAndView allProjects() {
+        ModelAndView modelAndView = new ModelAndView("/project/projects");
+        modelAndView.addObject("projects",
+                projectService.getProjectDtosCreatedByUser(SecurityUtil.getCurrentUser()));
+        return modelAndView;
+    }
+
+    @GetMapping("/project/add")
+    public ModelAndView addProject() {
+        ModelAndView modelAndView = new ModelAndView("/project/create-project");
+        modelAndView.addObject("project", new ProjectDto());
+        return modelAndView;
+    }
+
+    @PostMapping("/project/save")
+    public RedirectView saveProject(@ModelAttribute("project") ProjectDto projectDto) {
+        projectService.addProject(projectDto);
+        return new RedirectView("/projects");
+    }
+
+    @GetMapping("/project/{id}/edit")
+    public ModelAndView editProject(@PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView("/project/edit-project");
+        modelAndView.addObject("project", projectService.getDtoById(id));
+        return modelAndView;
+    }
+
+    @PostMapping("/project/update")
+    public RedirectView updateProject(@ModelAttribute("project") ProjectDto projectDto) {
+        projectService.updateProject(projectDto);
+        return new RedirectView("/projects");
+    }
+
+    @GetMapping("/projects/participated")
+    public ModelAndView myProjects() {
+        ModelAndView modelAndView = new ModelAndView("/project/projects-participated");
+        modelAndView.addObject("projects",
+                projectService.getProjectByUserParticipated(SecurityUtil.getCurrentUser()));
+        return modelAndView;
+    }
+
+    @GetMapping("/project/{id}")
+    public ModelAndView projectDetails(@PathVariable int id) {
+        ModelAndView modelAndView = new ModelAndView("/project/project-details");
+        modelAndView.addObject("project", projectService.getDtoById(id));
+        return modelAndView;
+    }
+
+
 }
