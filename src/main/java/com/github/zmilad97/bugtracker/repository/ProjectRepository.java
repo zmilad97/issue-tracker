45148eb3f98ec39bac45134b4584e593e0b81035
milad <zmilad97@gmail.com>
2021-09-19 12:36:43 +0430

    added Methods to ProjectRepository

--- src/main/java/com/github/zmilad97/bugtracker/repository/ProjectRepository.java
+++ src/main/java/com/github/zmilad97/bugtracker/repository/ProjectRepository.java
@@ -1,4 +1,17 @@
 package com.github.zmilad97.bugtracker.repository;
 
-public class ProjectRepository {
+import com.github.zmilad97.bugtracker.model.Project;
+import com.github.zmilad97.bugtracker.model.Team;
+import com.github.zmilad97.bugtracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    List<Project> getAllByCreator(User user);
+
+    Project getProjectById(int id);
+
+    List<Project> getProjectsByTeam(Team team);
+
 }
