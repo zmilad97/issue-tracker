6a4afb49ea8fff9ad2d32cc6b1bd1dfbf096cf2e
milad <zmilad97@gmail.com>
2021-09-19 12:36:25 +0430

    fixed PackageDto

--- src/main/java/com/github/zmilad97/bugtracker/dtos/ProjectDto.java
+++ src/main/java/com/github/zmilad97/bugtracker/dtos/ProjectDto.java
@@ -1,4 +1,85 @@
 package com.github.zmilad97.bugtracker.dtos;
 
+import com.github.zmilad97.bugtracker.model.Team;
+import com.github.zmilad97.bugtracker.model.User;
+
+import javax.persistence.ManyToOne;
+
 public class ProjectDto {
+
+
+    private int id;
+
+    private String title;
+    private String description;
+    private int creatorId;
+    private int teamId;
+    private String creatorName;
+    private String teamName;
+    private String createdAt;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(int creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
