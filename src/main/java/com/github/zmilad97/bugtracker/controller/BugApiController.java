f3f1acfd1b4581e6ff30ca1ecc75709b7f5fb66a
milad <zmilad97@gmail.com>
2021-09-28 14:56:31 +0330

    added BugApiController

---
+++ src/main/java/com/github/zmilad97/bugtracker/controller/BugApiController.java
@@ -0,0 +1,26 @@
+package com.github.zmilad97.bugtracker.controller;
+
+import com.github.zmilad97.bugtracker.dtos.BugDto;
+import com.github.zmilad97.bugtracker.service.BugService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api")
+public class BugApiController {
+    private final BugService bugService;
+
+
+    public BugApiController(BugService bugService) {
+        this.bugService = bugService;
+    }
+
+    @GetMapping("/bug/{id}")
+    public BugDto getBug(@PathVariable int id) {
+        return bugService.getBugDto(id); //TODO: make it not to access from other teams
+    }
+
+
+}
