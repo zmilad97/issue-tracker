f6a888413ee4f556f62cde6a395ed830ed1faf5b
milad <zmilad97@gmail.com>
2021-10-12 12:17:03 +0330

    fixed project error handling

--- src/main/java/com/github/zmilad97/bugtracker/controller/ProjectController.java
+++ src/main/java/com/github/zmilad97/bugtracker/controller/ProjectController.java
@@ -6,11 +6,14 @@ import com.github.zmilad97.bugtracker.service.ProjectService;
 import com.github.zmilad97.bugtracker.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
@@ -37,19 +40,24 @@ public class ProjectController {
     }
 
     @GetMapping("/project/add")
-    public ModelAndView addProject() {
-        ModelAndView modelAndView = new ModelAndView("project/create-project");
-        modelAndView.addObject("project", new ProjectDto());
-        modelAndView.addObject("teams", teamService.getTeamDtoByUser(SecurityUtil.getCurrentUser()));
-        modelAndView.addObject("sideBarProjects",
+    public String addProject(Model model) {
+        model.addAttribute("project", new ProjectDto());
+        model.addAttribute("teams", teamService.getTeamDtoByUser(SecurityUtil.getCurrentUser()));
+        model.addAttribute("sideBarProjects",
                 projectService.getProjectByUserParticipated(SecurityUtil.getCurrentUser()));
-        return modelAndView;
+        return "project/create-project";
     }
 
     @PostMapping("/project/save")
-    public RedirectView saveProject(@ModelAttribute("project") @Valid ProjectDto projectDto) {
+    public String saveProject(Model model, @Valid @ModelAttribute("project") ProjectDto projectDto, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            model.addAttribute("project", projectDto);
+            model.addAttribute("team", teamService.getTeamDtoByUser(SecurityUtil.getCurrentUser()));
+
+            return "project/create-project";
+        }
         projectService.addProject(projectDto);
-        return new RedirectView("/projects");
+        return "redirect:/projects";
     }
 
     @GetMapping("/project/{id}/edit")
@@ -63,9 +71,14 @@ public class ProjectController {
     }
 
     @PostMapping("/project/update")
-    public RedirectView updateProject(@ModelAttribute("project") ProjectDto projectDto) {
+    public String updateProject(@ModelAttribute("project") @Valid ProjectDto projectDto, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("project", projectDto);
+            model.addAttribute("teams", teamService.getTeamDtoByUser(SecurityUtil.getCurrentUser()));
+            return "project/edit-project";
+        }
         projectService.updateProject(projectDto);
-        return new RedirectView("/projects");
+        return "redirect:/projects";
     }
 
     @GetMapping("/projects/participated")
